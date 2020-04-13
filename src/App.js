798b97807ad090694be056484b3fb1be5ce02c5d
Jacob-Ruleaux <jakeruleaux@gmail.com>
2020-04-13 10:17:03 -0700

    generate splash screen comp for splash screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Splash from './Splash';
 
 
 function App() {
   return (
     <div className="App">
-    
+      <h1>Start</h1>
+      <Splash />
     </div>
   );
 }
---
+++ src/Splash.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class Splash extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            backgroundColor: ''
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                spalsh
+            </div>
+        );
+    }
+}
+
+export default Splash;
