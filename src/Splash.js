3cfe5f83be70a38ba87f63b054ef72a3674badd2
Jacob-Ruleaux <jakeruleaux@gmail.com>
2020-04-15 10:52:29 -0700

    import megrim font to host it and prevent load delay

--- src/Splash.js
+++ src/Splash.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import styled, { keyframes } from 'styled-components'
 import './App.css';
 
 class Splash extends Component {
@@ -8,28 +9,32 @@ class Splash extends Component {
             backgroundColor: ''
         }
     }
+    // <div style={styles.splashContainer} className="spashContainer">
+    //             <span className="megrimFont" style={styles.logo}>*</span>
+    //         </div>
     render() {
 
-        const styles = {
-            splashContainer: {
-                width: '100vw',
-                height: '100vh',
-                position: 'absolute',
-                top: '0',
-                left: '0',
-                display: 'flex',
-                justifyContent: 'center'
-            },
-            logo: {
-                fontSize: '1000px',
-            }
-            
-        }
+        const background = keyframes`
+            from {background-color: red;}
+            to {background-color: yellow;}
+        `;
+
+        const SplashContainer = styled.div `
+                width: 100vw;
+                height: 100vh;
+                position: absolute;
+                top: 0;
+                left: 0;
+                display: flex;
+                justify-content: center;
+                background-color: red;
+                font-size: 1000px;
+                animation: ${background} 4s;
+                font-family: Megrim;
+        `;
         
         return(
-            <div style={styles.splashContainer} className="spashContainer">
-                <span className="megrimFont" style={styles.logo}>*</span>
-            </div>
+            <SplashContainer>*</SplashContainer>
         );
     }
 }
