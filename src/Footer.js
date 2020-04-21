a72254c4f31aece18fe43b7996aac11bb547327a
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-04-21 10:56:40 -0700

    add mailto icon and link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,9 @@ import styled, { keyframes } from 'styled-components';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons' ;
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons' ;
 
-library.add(faInstagram);
-
-
+library.add(faInstagram, faEnvelope);
 
 const Footer = () => {
     const [ year, setYear] = React.useState('');
@@ -16,22 +15,46 @@ const Footer = () => {
         bottom: 0;
         width: 100vw;
         display: flex;
-        flex-wrap: no-wrap;
+        flex-wrap: nowrap;
+        justify-content: flex-end;
     `;    
 
     const FooterText = styled.p `
         font-family: Megrim;
         color: #ededed;
+        margin-right: 20px;
     `;
     
     const getYear = () => {
         return new Date().getFullYear();
     };
 
+    const instagramLink = () => {
+        return (
+            <a href="https://www.instagram.com/?hl=en" target="blank" />
+        );
+    };
+
+    const emailLink = () => {
+        // window.location.href = `mailto:themorningstarbus@gmail.com`;
+    }
+
     return (
         <Footer>
             <FooterText>© {getYear()} morningStar, LLC.</FooterText>
-            <FontAwesomeIcon icon={faInstagram} style={{ color: '#ededed', fontSize: '30px'}} />
+            <FontAwesomeIcon 
+                icon={faInstagram}
+                style={{ color: '#ededed', fontSize: '30px', marginRight: '20px'}}
+            >
+            </FontAwesomeIcon>
+            <a href="mailto:themorningstarbus@gmail.com?Subject=Hello%20again" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon 
+                    icon={faEnvelope}
+                    style={{ color: '#ededed', fontSize: '30px', marginRight: '20px'}}
+                    onClick={emailLink()}
+                >
+                </FontAwesomeIcon>
+            </a>           
         </Footer>
     );
 }
