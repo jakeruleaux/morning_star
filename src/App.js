ac0d61559ed87013638918037f4ebda29b4a8c79
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-04-21 09:48:03 -0700

    import react font awesome packeages for social icon useage in footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Splash from './Splash';
+import Footer from './Footer';
 
 
 function App() {
   return (
     <div>
       <Splash />
+      <Footer />
     </div>
   );
 }
