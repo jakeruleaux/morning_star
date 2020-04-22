d03d1037c02106d6cbbff821eb7dee14e76c2ad5
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-04-22 11:13:31 -0700

    change router package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router";
 import Splash from './Splash';
 import Footer from './Footer';
+import Admin from './Admin';
 
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div>
       <Splash />
       <Footer />
+      <Switch>
+        <Route exact path="/admin" component={Admin} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
