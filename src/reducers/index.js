a3860b42fe26d8854fbf48a969b08d7e08fec383
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-05-06 10:57:16 -0700

    begin admin reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { reducer } from 'redux-form';
+import isadminReducer from './isadminReducer';
 
 
 export default combineReducers({
+   isAdmin: isadminReducer,
    form: reducer
 });
---
+++ src/reducers/isadminReducer.js
@@ -0,0 +1,14 @@
+import { IS_ADMIN } from "../actions/types";
+
+const INITIAL_STATE = {
+    isAdmin: null,
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case IS_ADMIN:
+            return { ...state, isAdmin: true };
+        default:
+            return state;
+    }
+};
