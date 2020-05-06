cb89edd3217fd10d5188f1facd60fa1a896bd1f2
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-05-06 10:03:18 -0700

    fix promise in google auth function to filter two emails as admin only

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -15,15 +15,15 @@ class Admin extends Component {
         const firebase = firebaseApp;
         const provider = new firebase.firebase_.auth.GoogleAuthProvider();     
         firebase.firebase_.auth().signInWithPopup(provider).then(function(result) {
-            // const  token = result.credential.accessToken;
             const user = result.user;
+            return user;
           })
           .then(user => {
             // filter emails to prevent unauthorized login
-            console.log(user, '?')
-            if(user.email === 'jakeruleaux@gmail.com') {
+            if(user.email === 'jakeruleaux@gmail.com' || user.email === 'themorningstarbus@gmail.com') {
                 this.setState({ isAdmin: true });
-                console.log(user, 'user', user.email, this.state.isAdmin)
+            } else {
+                ///error
             }
           })
           .catch(function(error) {
@@ -32,8 +32,6 @@ class Admin extends Component {
     };
 
     render() {
-        // const firebase = firebaseApp;
-        console.log(this.state.isAdmin, 'admin in render')
         return (
            <div>
                <AdminLogin handleAdminLogin={this.handleAdminLogin} />
